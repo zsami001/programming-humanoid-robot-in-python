83c5277894bfea713be74bb13551c73169466b1e
zsami001 <zsami001@gmail.com>
2018-05-10 16:57:31 +0200

    assignment 1
    assignment 1 done

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -21,7 +21,8 @@ class MyAgent(SparkAgent):
         temperature = 0
         # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
-        print str (angel)
+        angle=perception.joint['HeadYaw']
+        temperature=perception.joint_temperature['HeadYaw']
 
         print 'HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature)
         return super(MyAgent, self).think(perception)
--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -22,7 +22,7 @@ class MyAgent(SparkAgent):
         action = super(MyAgent, self).think(perception)
         # YOUR CODE HERE
         action.stiffness['LShoulderPitch']=0
-	action.speed['HeadYaw']=0.1
+        action.speed['HeadYaw']=0.1
 
         return action
 
