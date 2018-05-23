9b8998e960aff8f785f549243bd9eb3d36d64d5d
zsami001 <zsami001@gmail.com>
2018-05-23 23:57:09 +0200

    Update angle_interpolation.py

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -41,8 +41,56 @@ class AngleInterpolationAgent(PIDAgent):
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
+        names, times, keys = keyframes
+        if self.animation_start_time < 0:
+            self.animation_start_time = perception.time
+            
+        curr_time = perception.time - self.animation_start_time
+        
+        for i, name in enumerate(names):
+            
+            if not(name in perception.joint):
+                continue
+            
+            for j in range(len(times[i])-1):
+                
+                if curr_time <= times[i][j]:
+                    point_1 = perception.joint[name]
+                    point_2 = keys[i][j][0]
+                    right_derivative_point_1 = 0.0
+                    left_derivative_point_2 = keys[i][j][2][2]
+                    c_1, c_2, c_3, c_4 = self.control_points(point_1, point_2, right_derivative_point_1, left_derivative_point_2)
+                    time = self.time_in_range_zero_to_one(self.animation_start_time, times[i][j], curr_time)
+                    target_joints[name] = self.bezier_interpolation(c_1, c_2, c_2, c_4, time)
+                    break
+                    
+                elif curr_time > times[i][j] and curr_time <= times[i][j+1]:
+                    point_1 = keys[i][j][0]
+                    point_2 = keys[i][j+1][0]
+                    right_derivative_point_1 = keys[i][j+1][2][2]
+                    left_derivative_point_2 = keys[i][j][2][2]
+                    c_1, c_2, c_3, c_4 = self.control_points(point_1, point_2, right_derivative_point_1, left_derivative_point_2)
+                    time = self.time_in_range_zero_to_one(times[i][j], times[i][j+1], curr_time)
+                    target_joints[name] = self.bezier_interpolation(c_1, c_2, c_2, c_4, time)
+                    break
+                
+                else:
+                    continue
 
         return target_joints
+    
+    def bezier_interpolation(self, control_1, control_2, control_3, control_4, time):
+        
+        return ((1-time)**3) * control_1 + 3* time * ((1-time)**2) * control_2 + 3 * (time**2) * (1-time) * control_3 + (time**3) * control_4
+
+    #4 control points for cubic bezier interpolation
+    def control_points(self, point_1, point_2, right_derivative_point_1, left_derivative_point_2):
+        
+        return point_1, point_1 + (right_derivative_point_1/3), point_2 + (left_derivative_point_2/3), point_2
+    
+    def time_in_range_zero_to_one(self, start_time, end_time, time):
+        
+        return (time - end_time) / (end_time - start_time)
 
 if __name__ == '__main__':
     agent = AngleInterpolationAgent()
